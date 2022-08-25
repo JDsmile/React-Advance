b7392d489726ca06cf89c9a56ff6158adb9a6d79
Yusen Jiang <yusenjia@buffalo.edu>
2022-08-24 17:21:01 -0700

    functional compoent stopwatch

--- a7-stop-watch/src/App.js
+++ a7-stop-watch/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import StopWatch from "./component/StopWatchFn";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <StopWatch />
+    </div>
+  );
 }
 
 export default App;
---
+++ a7-stop-watch/src/component/StopWatchFn.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function StopWatch() {
+  const [timer, setTimer] = React.useState(0);
+  const [startTimer, setStartTimer] = React.useState(false);
+
+  console.log("timer", timer);
+  console.log(startTimer);
+
+  React.useEffect(() => {
+    let interval = null;
+    if (startTimer) {
+      interval = setInterval(() => {
+        setTimer((prev) => prev + 10);
+      }, 10);
+    }
+    return () => clearInterval(interval);
+  }, [startTimer]);
+  return (
+    <>
+      <div className="timer__container">
+        <span>{("0" + (Math.floor(timer / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + (Math.floor(timer / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((timer / 10) % 1000)).slice(-2)}</span>
+        <br />
+        <button
+          onClick={() => {
+            setStartTimer(true);
+          }}
+        >
+          START
+        </button>
+        <button
+          onClick={() => {
+            setStartTimer(false);
+          }}
+        >
+          PAUSE
+        </button>
+        <button
+          onClick={() => {
+            setStartTimer(false);
+            setTimer(0);
+          }}
+        >
+          RESET
+        </button>
+      </div>
+    </>
+  );
+}
